89f445f884e08d695fda309232edd16dd8061d38
Magomed <jupiler_95@mail.ru>
2023-11-02 17:56:43 +0300

    исправил cakes

--- Cakes.cs
+++ Cakes.cs
@@ -1,33 +1,40 @@
+public class Cakes
+{
+    public static void Start()
+    {
+        // 0 Создать цены и название всех тортов    
+        string tort1 = "тортик";
+        string tort2 = "тортище";
+        string tort3 = "торт95";
 
+        int tort1cena = 1507;
+        int tort2cena = 3600;
+        int tort3cena = 200;
 
-    // 0 Создать цены и название всех тортов
-    
-    string tort1 = "тортик";
-    string tort2 = "тортище";
-    string tort3 = "торт95";
-    string tortino = "Тортиньо яц нету давай досвидание";
+        //1 Вывести текст (Муьлха торт еза хьуна?;)
+        Console.Write("Муьлха торт еза хьуна?: ");
 
+        //2 Получить название торта
+        //3 Сохранить название торта
+        string otvetpozovatelya = Console.ReadLine();
 
-    int tort1cena = 1507;
-    int tort2cena = 3600;
-    int tort3cena = 200;
+        //4 Сравнить название торта с имеюшимися тортами
 
-    //1 Вывести текст (Муьлха торт еза хьуна?;)
-    Console.Write("Муьлха торт еза хьуна?: ");
-
-    //2 Получить название торта
-    //3 Сохранить название торта
-
-    string otvetpozovatelya = Console.ReadLine();
-
-    //4 Сравнить название торта с имеюшимися тортами
-
-    if (otvetpolzovatelya. == tort1){
-        Console.WriteLine($"{tort1}ax {tort1cena} сом доьху")};
-        {Console.WriteLine($"{tort2}ax {tort2cena} сом доьху")};
-        {Console.WriteLine($"{tort3}ax {tort3cena} сом доьху")};
-
-    else {Console.WriteLine("{moyotvet} йяц, нету давай досвидания");
-
-    
+        if (otvetpozovatelya == tort1)
+        {
+            Console.WriteLine($"{tort1}ax {tort1cena} сом доьху");
+        }
+        else if (otvetpozovatelya == tort2)
+        {
+            Console.WriteLine($"{tort2}ax {tort2cena} сом доьху");
+        }
+        else if (otvetpozovatelya == tort3)
+        {
+            Console.WriteLine($"{tort3}ax {tort3cena} сом доьху");
+        }
+        else
+        {
+            Console.WriteLine("{moyotvet} йяц, нету давай досвидания");
+        }
     }
+}
