8514e5f26b9ad939bab59cecf9a82454a0b781a1
Magomed <jupiler_95@mail.ru>
2023-11-04 16:52:35 +0300

    SravnenieDvuhChisel

--- SravnenieDvuhChisel.cs
+++ SravnenieDvuhChisel.cs
@@ -1,7 +1,15 @@
-public class SravnenieDvuhChisel{
+using Microsoft.VisualBasic;
+
+public class  SravnenieDvuhChisel{
 
     public static void Start(){
-        Console.WriteLine("привет");
+        Console.WriteLine("по любому");
+        var a = 4;
+        var b = 6;
+        if (b > a)
+        Console.WriteLine("b больше");
+        else Console.WriteLine("a больше");
     }
+
 }
 
