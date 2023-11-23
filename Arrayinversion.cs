1d87055f1d09142a835cdf9dbbee92206ecd3595
Magomed <jupiler_95@mail.ru>
2023-11-23 17:35:02 +0300

    arraySdelal

--- Arrayinversion.cs
+++ Arrayinversion.cs
@@ -1,13 +1,7 @@
-public class Arrayinversion
+internal  class Arrayinversion
 { 
     public static void Start()
     {
-    int[] array = {12, 4, 3, -2, 1, 9, 0,};
-    for (int i = array.Length - 1; i >= 0; --i)
-    Console.Write(array[i] + " ");
-    Console.ReadKey(true);
-    }
-} 
 
 //Дан массив из целых чисел [12, 4, 3, -2, 1, 9, 0], напиши программу которая:
 //a) выведет элементы в обратном порядке, через запятую.
@@ -16,3 +10,16 @@ public class Arrayinversion
 //Нельзя использовать Array.Reverse.
 //
 //Структуру для всех задач в этом проекте делай на подобии Cakes.cs - внутри Start()
+
+
+        int[] chisla = {12, 4, 3, -2, 1, 9, 0 };
+        int vtoroiElement = chisla[1];
+        for(int i = 0; i < chisla.Length; i++)
+        {
+            int chislo = vtoroiElement * chisla[i];
+            Console.Write(chislo + " ");
+        }
+    }
+
+
+} 
